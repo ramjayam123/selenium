e9301305fe751cd071652ca6b7c3897a65136849
ramjayam123 <121242673+ramjayam123@users.noreply.github.com>
2023-01-01 10:29:46 -0800

    initial

---
+++ Teast1.java
@@ -0,0 +1,20 @@
+package youtube;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Teast1 { 
+    static WebDriver driver;
+	public static void main(String[] args)  {
+	WebDriverManager.operadriver().setup();
+	WebDriver driver =new OperaDriver();
+	String url="https://www.facebook.com";
+	driver.get(url);
+    driver.manage().window().maximize();
+    
+	}
+	}
+
